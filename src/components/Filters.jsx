253b39e8286a7c68f881c680dbe3c84ebf1fabc2
yrgenius <yrgenius80@gmail.com>
2023-06-23 22:36:04 +0900

    feat: add layout

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,27 +6,51 @@ const Filters = () => {
         <section className={styles.filters}>
             <h1 className={styles.title}>фильтры:</h1>
             <button className={styles.button}>сбросить все фильтры</button>
-            <p>сортировать по:</p>
+            <p className={styles.subtitle}>сортировать по:</p>
             <select name="filter" id="filter">
                 <option value="name">жанр</option>
                 <option value="year">год</option>
                 <option value="price">цена</option>
             </select>
-            <p>год релиза:</p>
+            <p className={styles.subtitle}>год релиза:</p>
             <select name="year" id="year">
                 <option value="name">2020</option>
                 <option value="year">2021</option>
                 <option value="price">2022</option>
             </select>
-            <ul>
-                <li>боевик</li>
-                <li>приключения</li>
-                <li>мульт</li>
-                <li>комедия</li>
-                <li>криминал</li>
+            <ul className={styles.list}>
+
+                <li className={styles.item}>
+                    <input className={styles.checkbox} type='checkbox' name='filter_input' value={'боевик'} />
+                    <label className={styles.label} htmlFor="filter_input">боевик</label>
+                </li>
+                <li className={styles.item}>
+                    <input className={styles.checkbox} type='checkbox' name='filter_input' value={'боевик'} />
+                    <label className={styles.label} htmlFor="filter_input">боевик</label>
+                </li>
+                <li className={styles.item}>
+                    <input className={styles.checkbox} type='checkbox' name='filter_input' value={'боевик'} />
+                    <label className={styles.label} htmlFor="filter_input">боевик</label>
+                </li>
+                <li className={styles.item}>
+                    <input className={styles.checkbox} type='checkbox' name='filter_input' value={'боевик'} />
+                    <label className={styles.label} htmlFor="filter_input">боевик</label>
+                </li>
+                <li className={styles.item}>
+                    <input className={styles.checkbox} type='checkbox' name='filter_input' value={'боевик'} />
+                    <label className={styles.label} htmlFor="filter_input">боевик</label>
+                </li>
+                <li className={styles.item}>
+                    <input className={styles.checkbox} type='checkbox' name='filter_input' value={'боевик'} />
+                    <label className={styles.label} htmlFor="filter_input">боевик</label>
+                </li>
+
             </ul>
-            <button className={styles.button}>сбросить все фильтры</button>
-            <button className={styles.button}>сбросить все фильтры</button>
+            <div className={styles.button__wrapper}>
+                <button className={styles.nav_button}>назад</button>
+                <button className={styles.nav_button}>вперед</button>
+            </div>
+
             <p className={styles.pagination}>1 of 1455</p>
         </section>
     );
